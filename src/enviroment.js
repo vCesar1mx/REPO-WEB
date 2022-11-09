05bf280f7a73819947f04867a71f0632b1e407c7
vCesar1mx <cesarvaz.e7@gmail.com>
2022-11-08 21:49:55 -0600

    CSS changes

--- src/enviroment.js
+++ src/enviroment.js
@@ -1,11 +1,10 @@
 const { running_mode } = require('../config.json');
 function onRun(res) {
-    if (running_mode == "debug") return res.set({ 'Running': 'debug', 'Supervised': 'Vazquez Cesar', 'X-Powered-By': 'Private-Eng.', 'Version': '1.0', 'Showing': 'Real-time' })
+    if (running_mode == "debug") return res.set({ 'Running': 'debug', 'Supervised': 'Vazquez Cesar', 'X-Powered-By': 'Private-Eng.', 'Version': '1.0', 'Showing': 'Real-time' });
 }
 function firstRun(app, bodyParser, morgan, compression) {
     if (running_mode == "debug") {
         app.use(morgan('dev'), compression());
-
     } else {
         app.use(morgan('combined'), compression());
     }
